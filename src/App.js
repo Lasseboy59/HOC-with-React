b3620e1c04280913859cdf5225d869b22eb1b626
Lauri Kyttälä <lauri.kyttala@gmail.com>
2020-09-09 12:57:51 +0300

    reading JSON data without HOC

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import UserProfile from './components/user-profile/user-profile.component';
+import UserList from './components/user-list/user-list.component';
+
+import './App.scss';
+
+function App() {
+  return (
+    <div className='App'>
+      <UserList />
+      <UserProfile
+        name='Yihua'
+        email='yihuazhang@gmail.com'
+      />
+    </div>
+  );
+}
+
+export default App;
