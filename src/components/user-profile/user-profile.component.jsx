c630d2e9bbb69444822badae4ef3bb9a30dcbc4b
Lauri Kyttälä <lauri.kyttala@gmail.com>
2020-09-09 14:20:03 +0300

    userProfile read with HOC-component

--- src/components/user-profile/user-profile.component.jsx
+++ src/components/user-profile/user-profile.component.jsx
@@ -1,38 +1,20 @@
 import React from 'react';
+import withData from '../../with-data.js';
 
-class UseProfile extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      posts: []
-    }
-  }
-
-  componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/posts')
-      .then(response => response.json())
-      .then(data => this.setState({ posts: data.slice(0, 3) }))
-  }
-
-  render() {
-    return (
-      <div className='container'>
-        <h1>{this.props.name}</h1>
-        <h2>{this.props.email}</h2>
+const UserProfile = ({ data, name, email }) => (
+  <div className='container'>
+    <h1>{name}</h1>
+    <h2>{email}</h2>
         Posts:
-        {
-          this.state.posts.map(post => (
-            <div className='post' key={post.id} >
-              <h1>{post.title}</h1>
-              <h2>{post.body}</h2>
-            </div>))
-        }
-      </div>
-    );
-  }
+    {data.map(post => (
+      <div className='post' key={post.id} >
+        <h1>{post.title}</h1>
+        <h2>{post.body}</h2>
+      </div>))
+    }
+  </div>
+);
 
-
-}
-
-export default UseProfile;
+export default withData(UserProfile,
+  'https://jsonplaceholder.typicode.com/posts'
+);
